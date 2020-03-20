da77d3829e440c37090ada4c7897e846bd1ce937
botris <me@borissondagh.com>
2020-03-20 12:25:42 +0100

    Hook factory into Rest classes, allow for code completion.

--- Factory.php
+++ Factory.php
@@ -3,7 +3,18 @@
 namespace Nsls\Moodle;
 
 use Nsls\Moodle\Http\Client;
+use Nsls\Moodle\Rest\Courses;
+use Nsls\Moodle\Rest\Groups;
+use Nsls\Moodle\Rest\Rest;
 
+/**
+ * Class Factory
+ *
+ * @method Courses    courses()
+ * @method Groups     groups()
+ *
+ * @package Nsls\Moodle
+ */
 class Factory
 {
 
@@ -16,6 +27,20 @@ class Factory
     public function __construct(string $token = null,  string $serverUrl = null, Client $client = null, array $httpClientArguments = [])
     {
         $this->client = $client ?: new Client($token, $serverUrl, null, $httpClientArguments);
+    }
+
+    /**
+     * Return an instance of a Rest class bases on method called.
+     *
+     * @param string $name
+     * @param array $arguments
+     *
+     * @return Rest
+     */
+    public function __call(string $name, array $arguments)
+    {
+        $restCall = 'Nsls\\Moodle\\Rest\\' . ucfirst($name);
+        return new $restCall($this->client);
     }
 
     public static function instantiate(string $token = null,  string $serverUrl = null, Client $client = null, array $httpClientArguments = []): Factory
